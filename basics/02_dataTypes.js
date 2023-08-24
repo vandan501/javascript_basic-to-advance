a7cd8b65c290cfe0d7f2905c52d69a9a2de8feaa
Raval Vandan <vandanraval2002@gmail.com>
2023-08-25 00:41:10 +0530

    datatypes file added

---
+++ basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict";// treat all js code as newer version or JS
+
+// alert( 3 + 2);//we are using node js not browser;
+
+console.log(9
+    +1); //code redability must be high  
+console.log("hello")
+
+/*
+Documentation for javascript
+1-> mdn
+2->tc39.es
+*/
+
+let name="vandan";//String 
+let age=21;//number
+let isLoggedIn=false;//boolean
+
+/*   Primitive datatypes */
+
+// number====->2 to power 53
+// bigInt----->in large amount of numbers
+// String------>""
+// boolean----->true /  false
+// null  ------>standalone value
+// undefined-- >value not assigned yet
+// symbol----->unique 
+
+
+/**OBJECT***/
+console.log(typeof "hello")//--->String
+console.log(typeof null)//--->OBJECT
+console.log(typeof undefined)//--->undefined
