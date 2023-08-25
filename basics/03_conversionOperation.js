1741d44c6e8a86f16a8840b73474904efd052bab
Raval Vandan <vandanraval2002@gmail.com>
2023-08-26 00:22:25 +0530

    conversion and operation added

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,57 @@
+let score1 = "33"; //--->String type
+let score2 = 33;  //-->Number type
+
+
+
+let valueInNumber = Number(score1); // it will convert string into number data type
+
+/*console.log(typeof score1);   
+console.log(typeof(score2));
+console.log(typeof valueInNumber);
+
+
+// "33"--->33
+// "33abc"-->NaN--Not a Number
+// true---->1
+// false---->0
+// null--->0
+// undefined--->NaN
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn=Boolean(isLoggedIn);//--->true means 1
+console.log("********************************")
+console.log(booleanIsLoggedIn)
+
+// 1-->true ;0--->false
+// "" =>false
+// "vandan"-->true
+
+let someNumber = 65;
+let StringNumber=String(someNumber);
+console.log("********************************")
+
+console.log(StringNumber)
+console.log(typeof StringNumber)
+
+*/
+
+
+/*********************OPERATIONS***************** */
+
+let value = 3;
+let negValue= -value;
+console.log(negValue);
+
+
+let str1 = "hello";
+let str2 = " vandan"
+let str3 = str1 + str2;
+console.log(str3)
+
+
+
+console.log("1" + 2);  //12
+console.log(1 + "2");  //12
+console.log("1" + 2 + 2);  //122
+console.log(1 + 2 + "2")  //32
